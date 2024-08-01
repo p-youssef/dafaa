54babb85f9912e6843ce65bb07b500f9e06b671d
philip <credom.it@gmail.com>
2024-08-01 14:22:16 +0200

    first commit 3

--- dafaa/settings.py
+++ dafaa/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=d#zusf+(eavfbx)!*eiwslgmc=yhx(*2+(42rs3oi8^1zj(ag'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.48.195.126',]
 
 
 # Application definition
